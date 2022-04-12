1e7e7bb56079bdf8524a35e2b7aa370a60fe3214
admin <admin@YD2-17>
2022-04-12 17:45:47 +0900

    20220412 수정

--- P20220412/src/co/edu/condition/Dice.java
+++ P20220412/src/co/edu/condition/Dice.java
@@ -1,12 +1,25 @@
 package co.edu.condition;
 
+import java.util.Scanner;
+
 public class Dice {
 
 	public static void main(String[] args) {
-		int dice = (int)(6 * (Math.random())+1);
-	
+		Scanner scn = new Scanner(System.in);
+		int my = scn.nextInt();
 
-	System.out.println("나온 주사위의 눈은 "+dice+"입니다");
+		System.out.println("숫자를 입력해주세요 ( 1 ~ 6 )");
+		int dice = (int) (6 * (Math.random()) + 1);
 
+		if (my == dice) {
+			System.out.println("입력하신 값은 " + my + "입니다");
+			System.out.println("나온 주사위의 눈은 " + dice + "입니다");
+			System.out.println("같습니다.");
+		} else {
+			System.out.println("입력하신 값은 " + my + "입니다");
+			System.out.println("나온 주사위의 눈은 " + dice + "입니다");
+			System.out.println("다릅니다.");
+
+		}
 	}
 }
--- P20220412/src/co/edu/loop/Star.java
+++ P20220412/src/co/edu/loop/Star.java
@@ -3,11 +3,13 @@ package co.edu.loop;
 public class Star {
 	public static void main(String[] args) {
 		for (int i = 0; i <= 4; i++) {
-			for (int j = i; j <= 4; j++) {
-				System.out.print("*");
+			for (int j = 0; j <= i; j++) {System.out.print("*");}
+			
+			for (int j = 0; j <4-i; j++) {System.out.print(" ");
+				
 			}
-			System.out.println("");
+			
+			System.out.println();
 		}
 	}
-
 }
